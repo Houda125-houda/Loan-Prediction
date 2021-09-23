db5de3c9ffcb7f373cf21c99d4c0e0d9939535f9
Houda125-houda <63107186+Houda125-houda@users.noreply.github.com>
2021-09-23 07:24:24 +0100

    update

---
+++ Loan prediction status.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 21:11:52 2021
+
+@author: LENOVO
+"""
+
+
+#####        Project Loan prediction      #####
+#####                                     #####
+###############################################
+
+# we are going to predict whether a person is eligible for a loan or not 
+# let's start :) 
+# we consider one finance company that gives loan for people so before approving the loan  this company analyzes various credential of the person 
+# se there are several aspects whether the person is educated, graduated or not , married or single so many parameters so this company wants to automate this loan approval process
+# so what happend, the person fill an online application form and based on the information given by the user we need to develop a machine learninh system that can tell the company of this person is elegiblefor this loan or not 
+
+
+# we are going to use the SVM support vector mOdel which is a supervised learning model ==>  we will give the data which has labels one is : loan approved and the second is rejected
